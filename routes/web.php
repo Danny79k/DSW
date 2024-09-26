e2231ed815810ac902b78aeebf8f7e0ef52d335e
Danny79k <belloli.danny@gmail.com>
2024-09-26 20:11:40 +0100

    creadas las rutas de analitycs y contacts

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/contacts', function() {
+    return view('contacts');
+})->middleware(['auth', 'verified'])->name('contacts');
+
+Route::get('/analitycs', function() {
+    return view('analitycs');
+})->middleware(['auth', 'verified'])->name('analitycs');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
