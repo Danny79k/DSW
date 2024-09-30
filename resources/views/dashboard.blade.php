3a318ef4adab4c5c439c2006142854272081b141
Danny79k <belloli.danny@gmail.com>
2024-09-30 18:27:20 +0100

    creadas las vistas de los community, el nombre del autor aparece al lado de sus contribuciones, creadas las relaciones entre las tablas users y community

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Here you will see the Community Links!") }}
+                <div class="p-6 text-gray-900 dark:text-gray-100 grid grid-rows-4 grid-cols-4 gap-3">
+                    @foreach ($links as $link)
+                    <div class="border-2 p-1 rounded-lg hover:bg-red-600 text-center">{{$link -> title}}<br><small class="text-slate-500">Contributed by: {{$link->creator->name}}</small><br><small class="text-slate-500"> {{$link->updated_at->diffForHumans()}}</small></div>
+                    @endforeach
+                    {{$links -> links()}}            
                 </div>
             </div>
         </div>
